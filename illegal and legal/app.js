9ac2101f3448a9b427f4595bad996f11da21f6bd
Shaheem-Ahmed <95481874+Shaheem-Ahmed@users.noreply.github.com>
2021-12-07 10:06:12 +0500

    Add files via upload

---
+++ illegal and legal/app.js
@@ -0,0 +1,9 @@
+var [a,b,c,d] = [0,1,2,3]
+
+document.write("Variable names can only contain ,numbers ,$  and _ For example $my_1stVariable")
+document.write("<br>")
+document.write("Variables must begin with a letter, $ or _. For example $name, _name or name")
+document.write("<br>")
+document.write("Variable Names are case sensitive")
+document.write("<br>")
+document.write("Variable Names should not be JS keywords")
