552df55e4b23624cce2eacea962812bcaf2616c9
Shaheem-Ahmed <95481874+Shaheem-Ahmed@users.noreply.github.com>
2021-12-07 18:46:07 +0500

    Add files via upload

--- ARRAYS/app.js
+++ ARRAYS/app.js
@@ -17,18 +17,53 @@ var mixedArray = ["Johnatan","5","3"]
 
 //boolean array//
 
-var matchFound = false;
- for (var i = 0; i <= 4; i++);
- if (cityToCheck === cleanestCities[i]) {
- matchFound = true;
- alert("It's one of the cleanest cities");
- }
- if (matchFound === false) {
- alert("It's not on the list");
- }
+//var numElements = cleanestCities.length;
+ //var matchFound = false;
+ //for (var i = 0; i < numElements; i++);
+ //if (cityToCheck === cleanestCities[i]) {
+// matchFound = true;
+ //alert("It's one of the cleanest cities");
  
+// }
+ 
+ //if (matchFound === false) {
+ //alert("It's not on the list");
+ //}
+
+
  //QUALIFICATIONS//
 
  var QUALIFICATIONS = ["SSC","HCS","BCS","BS","BCOM","MS","M.Phil","PhD"]
 
  console.log(QUALIFICATIONS)
+
+ //STUDENT ARRAY//
+
+ var surname = ["Michael","John","Tony"]
+ var score = ["230","320","480"]
+ var percentage = ["46%","64%","96%"]
+
+ var combined = []
+ combined.push(surname,score,percentage)
+
+ console.log(`combined = `+combined)
+
+ //single string//
+
+ var arr = ["This ", " is ", " my ", " cat"];
+ 
+document.write(arr[0]); 
+document.write(arr[1]); 
+document.write(arr[2]);
+document.write(arr[arr.length -1 ]); 
+
+////
+
+let numbers = [0, 1 , 2, 3, 10, 20, 30 ];
+numbers.sort( function( a , b){
+    if(a > b) return 1;
+    if(a < b) return -1;
+    return 0;
+});
+
+console.log(numbers);
--- Math Expression 2/app.js
+++ Math Expression 2/app.js
@@ -3,4 +3,116 @@
 var yourName = prompt("ENTER YOUR NAME")
 alert("Welcome "  + yourName)
 
-//number from user //
+//arithmetic //
+var num = prompt("ENTER NUMBER");
+var add = (++num);
+var sum = (num++);
+var minus = (--num);
+var subtract = (num--);
+
+document.write("RESULT")
+document.write("<br>")
+document.write("THE VALUE OF A IS " + num)
+document.write("<br>")
+document.write("<hr />");
+document.write("<br>")
+document.write("THE VALUE OF A IS: " + num)
+document.write("<br>")
+document.write("THE VALUE OF ++A IS: " + add)
+document.write("<br>")
+document.write("THE VALUE OF A IS: " + num)
+document.write("<br>")
+document.write("THE VALUE OF A++ IS: " + sum)
+document.write("<br>")
+document.write("<br>")
+document.write("THE VALUE OF --A IS: " + num)
+document.write("<br>")
+document.write("NOW THE VALUE OF A IS: " + minus)
+document.write("<br>")
+document.write("<br>")
+document.write("THE VALUE OF A-- IS: " + num)
+document.write("<br>")
+document.write("NOW THE VALUE OF A IS:" + subtract)
+
+//a, b & result after execution of the following script://
+
+var a = 2;
+var b = 1;
+var c = --a - --b + ++b + b--;
+
+console.log(c)
+
+//OUTPUT//
+
+var a = 2;
+var b = 1;
+var c = --a;
+var d = --a - --b;
+var e = --a - --b + ++b;
+var f = --a - --b + ++b + b--;
+
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+
+//MULTIPLICATION TABLE//
+
+var num = prompt("Enter Number", "5") 
+var num = parseInt(num); 
+var i = 0;
+document.write("<br>")
+for (i = 1; i < 11; i++) {
+  document.write("<tr><td><br>" + num + " x " + i + " = " + num * i + "</td></tr>");
+}
+
+//SUBJECT NAME//
+
+var subject1 = prompt("ENTER SUBJECT NAME");
+var subject2 = prompt("ENTER SUBJECT NAME");
+var subject3 = prompt("ENTER SUBJECT NAME");
+
+var num1 = 100;
+var num2 = 100;
+var num3 = 100;
+
+var marks = prompt("ENTER MARKS")
+var marks2 = prompt("ENTER MARKS")
+var marks3 = prompt("ENTER MARKS")
+
+var markOBTAINED = (num1 - marks);
+var markOBTAINED2 = (num2 - marks2);
+var markOBTAINED3 = (num3 - marks3);
+
+var percentage = ((markOBTAINED/100) * 100)
+var percentage2 = ((markOBTAINED2/100) * 100)
+var percentage3 = ((markOBTAINED3/100) * 100)
+
+document.write("<br>")
+
+document.write("<th> SUBJECT</th>")
+
+document.write("<th> TOTAL MARKS</th>")
+
+document.write("<th> OBTAINED MARKS</th>")
+
+document.write("<th> PERCENTAGE</th>")
+
+
+
+document.write(subject)
+document.write(subject2)
+document.write(subject3)
+
+document.write(marks)
+document.write(marks2)
+document.write(marks3)
+
+document.write(markOBTAINED)
+document.write(markOBTAINED2)
+document.write(markOBTAINED3)
+
+document.write(percentage)
+document.write(percentage2)
+document.write(percentage3)
+
